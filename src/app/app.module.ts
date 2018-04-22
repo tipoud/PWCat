3270513a11e7d612043de573bd5fa767c256a6e8
douay <douay.n@live.fr>
2018-04-22 16:11:44 +0200

    toolBar + img-Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatToolbar, MatToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
